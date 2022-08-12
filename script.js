f8cde3a8d6ec788a484dc6748ca0b9405bc67575
Alex <s.tsamalashvili1998@gmail.com>
2022-08-12 10:41:29 +0400

    added year prices

--- script.js
+++ script.js
@@ -2,49 +2,45 @@ const range = document.getElementById("range");
 const monthlyPrice = document.getElementById('monthlyPrice');
 const pageViews = document.getElementById('pageviews-count')
 const switcher = document.getElementById('switcher');
+const month = document.getElementById('month');
 let clickButton = false;
-let enter = 0;
+
 let rangeValue = 50 ;
 switcher.addEventListener('click',(event)=>{
-    if(enter %2 ===0){
-        clickButton = true;
-    }else{
-        clickButton = false;
-    }
-    enter++
+    clickButton = !clickButton;
     if(!clickButton){
         if(rangeValue>=0&&rangeValue<25){
             monthlyPrice.textContent = "$"+10+".00";
-            pageViews.textContent = 50+"K Pageviews";
+            month.textContent ="/month";
         }else if (rangeValue>=25&&rangeValue<50) {
             monthlyPrice.textContent = "$"+12+".00";
-            pageViews.textContent = 75+"K Pageviews";
+            month.textContent ="/month";
         } else if (rangeValue>=50&&rangeValue<=70){
             monthlyPrice.textContent = "$"+16+".00";
-            pageViews.textContent = 100+"K Pageviews";
+            month.textContent ="/month";
         }else if (rangeValue>70&&rangeValue<90){
             monthlyPrice.textContent = "$"+20+".00";
-            pageViews.textContent = 150+"K Pageviews";
+            month.textContent ="/month";
         }else{
             monthlyPrice.textContent = "$"+25+".00";
-            pageViews.textContent = 200+"K Pageviews";
+            month.textContent ="/month";
         }
     }else{
-        if(rangeValue>=0&&rangeValue<25){
-            monthlyPrice.textContent = "$"+7.5;
-            pageViews.textContent = 50+"K Pageviews";
-        }else if (rangeValue>=25&&rangeValue<50) {
-            monthlyPrice.textContent = "$"+9+".00";
-            pageViews.textContent = 75+"K Pageviews";
-        } else if (rangeValue>=50&&rangeValue<=70){
-            monthlyPrice.textContent = "$"+12.00+".00";
-            pageViews.textContent = 100+"K Pageviews";
-        }else if (rangeValue>70&&rangeValue<90){
-            monthlyPrice.textContent = "$"+15.00+".00";
-            pageViews.textContent = 150+"K Pageviews";
+        if(event.target.value>=0&&event.target.value<25){
+            monthlyPrice.textContent = "$"+90;
+            month.textContent ="/year";
+        }else if (event.target.value>=25&&event.target.value<50) {
+            monthlyPrice.textContent = "$"+108;
+            month.textContent ="/year";
+        } else if (event.target.value>=50&&event.target.value<=70){
+            monthlyPrice.textContent = "$"+144;
+            month.textContent ="/year";
+        }else if (event.target.value>70&&event.target.value<90){
+            monthlyPrice.textContent = "$"+180;
+            month.textContent ="/year";
         }else{
-            monthlyPrice.textContent = "$"+18.75;
-            pageViews.textContent = 200+"K Pageviews";
+            monthlyPrice.textContent = "$"+225;
+            month.textContent ="/year";
         }
     }
 })
@@ -70,20 +66,20 @@ range.addEventListener("input",(event)=>{
         }
     }else{
         if(event.target.value>=0&&event.target.value<25){
-            monthlyPrice.textContent = "$"+7.5;
-            pageViews.textContent = 50+"K Pageviews";
+            monthlyPrice.textContent = "$"+90;
+            month.textContent ="/year";
         }else if (event.target.value>=25&&event.target.value<50) {
-            monthlyPrice.textContent = "$"+9+".00";
-            pageViews.textContent = 75+"K Pageviews";
+            monthlyPrice.textContent = "$"+108;
+            month.textContent ="/year";
         } else if (event.target.value>=50&&event.target.value<=70){
-            monthlyPrice.textContent = "$"+12.00+".00";
-            pageViews.textContent = 100+"K Pageviews";
+            monthlyPrice.textContent = "$"+144;
+            month.textContent ="/year";
         }else if (event.target.value>70&&event.target.value<90){
-            monthlyPrice.textContent = "$"+15.00+".00";
-            pageViews.textContent = 150+"K Pageviews";
+            monthlyPrice.textContent = "$"+180;
+            month.textContent ="/year";
         }else{
-            monthlyPrice.textContent = "$"+18.75;
-            pageViews.textContent = 200+"K Pageviews";
+            monthlyPrice.textContent = "$"+225;
+            month.textContent ="/year";
         }
     }
     rangeValue = event.target.value;
