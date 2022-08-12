1a70d78a1628affae0bef2317dca78ff36077430
Alex <s.tsamalashvili1998@gmail.com>
2022-08-12 11:28:00 +0400

    fixed scrolling bug

--- script.js
+++ script.js
@@ -67,18 +67,23 @@ range.addEventListener("input",(event)=>{
         if(event.target.value>=0&&event.target.value<25){
             monthlyPrice.textContent = "$"+90;
             month.textContent ="/year";
+            pageViews.textContent = 50+"K Pageviews";
         }else if (event.target.value>=25&&event.target.value<50) {
             monthlyPrice.textContent = "$"+108;
             month.textContent ="/year";
+            pageViews.textContent = 75+"K Pageviews";
         } else if (event.target.value>=50&&event.target.value<=70){
             monthlyPrice.textContent = "$"+144;
             month.textContent ="/year";
+            pageViews.textContent = 100+"K Pageviews";
         }else if (event.target.value>70&&event.target.value<90){
             monthlyPrice.textContent = "$"+180;
             month.textContent ="/year";
+            pageViews.textContent = 150+"K Pageviews";
         }else{
             monthlyPrice.textContent = "$"+225;
             month.textContent ="/year";
+            pageViews.textContent = 200+"K Pageviews";
         }
     }
     rangeValue = event.target.value;
